a7a1ccf95b26799fbd13456f479f8b6711b41603
kranthi <kranthireddy.mekala@gmail.com>
2022-06-19 14:12:46 +0000

    react coding practices

---
+++ blogList/src/components/UserInfo/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      className="user-profile-image"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+    />
+    <h1>Wade Warren</h1>
+    <p className="user-description">software developer at uk</p>
+  </div>
+)
+export default UserInfo
